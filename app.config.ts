fda60415614d85507a348b6ed9554b7abef10e3c
liuyinghao <liuyinghao@leyifan.com>
2025-12-25 14:49:15 +0800

    ✨ feat: 更改名称

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { useI18n } from "vue-i18n";
 export default defineAppConfig({
   shadcnDocs: {
     site: {
-      name: "Inspira UI",
+      name: "Reborn UI",
       description: "Build beautiful websites using Vue & Nuxt.",
     },
     theme: {
