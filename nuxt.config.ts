540be7d18ef17886c87d953d1c72fb06d96dea88
liuyinghao <liuyinghao@leyifan.com>
2026-01-26 16:04:47 +0800

    fix: :bug: 打包路径修复

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -126,7 +126,7 @@ export default defineNuxtConfig({
     publicAssets: [
       {
         baseURL: "/docs/uni-render/",
-        dir: resolve(__dirname, "./packages/uniapp-project/dist/build/h5"),
+        dir: resolve("./packages/uniapp-project/dist/build/h5"),
         maxAge: 60 * 60 * 24 * 7,
       },
     ],
